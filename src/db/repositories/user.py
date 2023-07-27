f37838e2eb84bcd62181a35f74ab216f9e50db46
saneknapenek <kolesnikovag22@gmail.com>
2023-07-28 02:25:35 +0300

    add deactivate method in user repository

--- src/db/repositories/user.py
+++ src/db/repositories/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import select
+from uuid import UUID
+
+from sqlalchemy import select, update
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,8 +16,16 @@ class UserAlchemy(SQLAlchemyRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_for_username(self, username: str) -> DeclarativeBase:
+    async def get_for_login(self, login: str) -> DeclarativeBase:
         stmt = (select(self.model).
-                where(self.model.login==username))
+                where(self.model.login==login))
         res = await self.session.execute(stmt)
-        return res.first()
+        return res.scalar_one_or_none()
+    
+    async def deactivate(self, id: UUID) -> UUID:
+        stmt = (update(self.model).
+                where(self.model.id==id).
+                values(is_active=False).
+                returning(self.model.id))
+        res = await self.session.execute(stmt)
+        return res.scalar_one_or_none()

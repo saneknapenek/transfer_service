edef25067df5a3aacb86c8ff73c5e235b2f8c5db
saneknapenek <kolesnikovag22@gmail.com>
2023-08-10 22:59:09 +0300

    added SImage for hashing and getting metadate

--- yandex_service/src/utils/sync/hashing.py
+++ yandex_service/src/utils/sync/hashing.py
@@ -1,24 +1,69 @@
+from abc import ABC, abstractmethod
+
 from PIL import Image
 import imagehash
 import io
 
 
-class Hasher:
 
-    def image_hash(b_image: bytes) -> str:
-        image = Image.open(io.BytesIO(b_image))
-        hash = imagehash.average_hash(image)
-        return hash
+class Media(ABC):
+
+    GPSINFO = ...
+    DATETIME = ...
+
+    def __init__(self, obj: bytes):
+        self.obj = io.BytesIO(obj)
+
+    @abstractmethod
+    def _get_metadata(self) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_hash(self) -> str:
+        raise NotImplementedError
     
-    def video_hash(b_video: bytes) -> str:
-        pass
+    @property
+    def hash(self):
+        return self._get_hash()
+    
+    @property
+    def metadata(self):
+        return self._get_metadata()
 
 
-class ExifData:
+class SImage(Media):
 
-    GPSInfo = ...
-    latitude = ...
-    longitude = ...
+    GPSINFO = 34853
+    DATETIME = 306
 
-    def get_exif(obj: bytes) -> dict:
-        pass
+    def _get_hash(self) -> str:
+        image = Image.open(self.obj)
+        hash = imagehash.average_hash(image)
+        return str(hash)
+    
+    def _get_metadata(self) -> dict:
+        img = Image.open(self.obj)
+        exif_data = img._getexif()
+        try:
+            gps = exif_data[self.GPSINFO]
+            latitude = (2 / 3) * (gps[2][0] + gps[2][1] / 60 + gps[2][2] / 3600)
+            longitude = (2 / 3) * (gps[4][0] + gps[4][1] / 60 + gps[4][2] / 3600)
+        except KeyError:
+            latitude = None
+            longitude = None
+        return {
+            "GPSInfo": {
+                "latitude": latitude,
+                "longitude": longitude
+            },
+            "datetime": exif_data[self.DATETIME]
+        }
+
+
+class SVideo(Media):
+    
+    def _get_hash(self) -> str:
+        return super().get_hash()
+    
+    def _get_metadata(self) -> dict:
+        return super().get_metadata()
